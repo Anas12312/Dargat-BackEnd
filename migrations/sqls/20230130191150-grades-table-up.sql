60b3df062e96645991bde2d24d7cbba86ac72139
anazz12312 <anashesham000@gmail.com>
2023-01-30 21:16:50 +0200

    asdasd

---
+++ migrations/sqls/20230130191150-grades-table-up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE grades (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR ( 50 ),
+	sub1 int,
+	sub2 int,
+	sub3 int,
+	sub4 int,
+	sub5 int,
+	sub6 int,
+	total int,
+	percent int
+);
--- src/models/grade.ts
+++ src/models/grade.ts
@@ -1,18 +1,23 @@
 import Client from "../database";
 
-export type Book = {
-    id?:number,
-    title: string,
-    author: string,
-    totalpages: number,
-    summary: string
+export type Grade = {
+    id:number,
+    name:string,
+    sub1:number,
+    sub2:number,
+    sub3:number,
+    sub4:number,
+    sub5:number,
+    sub6:number,
+    total:number,
+    percent:number
 }
 
 export class GradeStore {
-    async index(): Promise<Book[]> {
+    async index(): Promise<Grade[]> {
         try {
             const conn = await Client.connect();
-            const sql = "SELECT * FROM books";
+            const sql = "";
             const result = await conn.query(sql);
             conn.release();
             return result.rows;
