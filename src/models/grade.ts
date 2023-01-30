eafba1a3be14b09d1ec60f0f70cf5a0657e194b1
Zyad Abdul-Nasser <zizo_007@live.com>
2023-01-30 22:12:08 +0200

    Z

--- src/models/grade.ts
+++ src/models/grade.ts
@@ -1,28 +1,35 @@
 import Client from "../database";
+import fs from 'fs'
+import csv from 'csv-parser'
 
 export type Grade = {
-    id:number,
-    name:string,
-    sub1:number,
-    sub2:number,
-    sub3:number,
-    sub4:number,
-    sub5:number,
-    sub6:number,
-    total:number,
-    percent:number
+    id: number,
+    name: string,
+    sub1: number,
+    sub2: number,
+    sub3: number,
+    sub4: number,
+    sub5: number,
+    sub6: number,
+    total: number,
+    percent: number
+}
+
+const  readData = async ():Promise<unknown[]> => {
+    let results:unknown[] = [];
+    const o = fs.createReadStream('data.csv');
+            o.pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => console.log(results));
+
+            console.log(o);
+    return results;
 }
 
 export class GradeStore {
-    async index(): Promise<Grade[]> {
-        try {
-            const conn = await Client.connect();
-            const sql = "";
-            const result = await conn.query(sql);
-            conn.release();
-            return result.rows;
-        }catch(err) {
-            throw new Error(`Failed: ${err}`);
-        }
+    index(): unknown[] {
+        const x = readData();
+        console.log("a7aaaaaaaaaaaaaaaaa");
+        return x;
     }
 }
