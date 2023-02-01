a31c6f3d57d13d3b1f36f464f9ddb07b52e9a6e4
Zyad Abdul-Nasser <zizo_007@live.com>
2023-02-01 21:43:38 +0200

    z

--- scrap.js
+++ scrap.js
@@ -1,14 +1,16 @@
 const ObjectsToCsv = require('objects-to-csv');
 const puppeteer =  require("puppeteer");
+const path = require('path');
 
 async function getData(start, end) {
     const browser = await puppeteer.launch({ headless:false })
     const page = await browser.newPage();
     const arr = [];
     for(let i=start;i <= end;i++) {
-        await page.goto(`http://app1.helwan.edu.eg/Computer/HasasnUpMview.asp?StdCode=${i}`);
+        await page.goto(`http://app1.helwan.edu.eg/Commerce/HasasnUpMview.asp?StdCode=${i}`);
         const data = await page.evaluate(() => {
             const record = Array.from(document.querySelectorAll('b')).map((x) => x.textContent);
+            const id =record[8];
             const subject1 = Number(record[42]);
             const subject2 = Number(record[56]);
             const subject3 = Number(record[70]);
@@ -18,7 +20,7 @@ async function getData(start, end) {
             const total = subject1+subject2+subject3+subject4+subject5+subject6;
             console.log(record[5])
             return {
-                id: record[8],
+                id,
                 name: record[5],
                 subject1,
                 subject2,
@@ -29,16 +31,18 @@ async function getData(start, end) {
                 total
             }
         })
-        arr.push(data);
+        if(Number(data.id) !== 0) arr.push(data);
     }
     browser.close();
     return arr;
 }
-const objToCsv = async (arr) => {
+const objToCsv = async (arr,i ,j) => {
     const csv = new ObjectsToCsv(arr);
-    await csv.toDisk('./test.csv');
+    await csv.toDisk(path.join(__dirname, `./Data/_${i}${j}.csv`));
 }
 const run = async (start, end, dep, year) => {
-    objToCsv(await getData(10001,10003));
+    const arr = await getData(start,end);
+    console.log(arr)
+    objToCsv(arr,dep, year);
 }
-run(10001,10003,0,2);
+run(285260,289689,0,0);
